a790b036e151e199a05a6d08ef8b426eafe37f94
Andrew Tanuwijaya <andrew.tanuwijaya7dev@gmail.com>
2020-08-11 03:47:32 +0700

    Convert HTML to Sevelte Components

---
+++ src/data/charities.js
@@ -0,0 +1,16 @@
+module.exports = { 
+    charities: [
+        {
+            id: 1,
+            title: 'First Charity Project',
+            category: 'Money',
+            thumbnail: '',
+            pledged: 99000,
+            target: 100000,
+            date_end: +new Date('10 September 2020'),
+            profile_photo: '',
+            profile_name: 'Viroide Bueno',
+            no_pledges: 0
+        }
+    ]
+}
